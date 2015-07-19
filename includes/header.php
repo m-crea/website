808498e5d6d09f90e9d71a7624156f0e82772c91
m-crea <em.hivert@gmail.com>
2015-07-19 12:15:53 +0200

    ajout top-header + homogénéisation header footer toutes pages

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<div id="header">
+        <div class="m-content">
+        	<div class="m-content_header clearfix">
+          		   <h1 id="home">
+                   		<a href="#global">
+                        	<img src="images/logo.png" alt="webdesigner et solutions artistiques digitales" ></a>
+                   </h1>
+                   <nav id="m-top_nav">
+                       <ul>
+                             
+                             <li class="nav-main goslide"><a href="#jms-slideshow"><img src="icones/actu.png" alt="actualites et introduction au webdesign" />Actus</a></li>
+                             <li class="nav-main main"><a href="#main"><img src="icones/m-real.png" alt="solutions artistiques digitales, interfaces ui web mobile et design print"/>Réalisations</a></li>
+                             <li class="nav-main main"><a href="profil.html"><img src="icones/profil.png" alt="connaître emilie hivert"/>Profil</a></li>
+                             <li class="nav-main cv"><a href="cv.pdf" class="m-btn m-btn_red" title="télécharger le cv" target="_blank">Voir le cv</a></li>
+                      </ul>
+    			   </nav>
+            <!--div id="logos">
+                 <div id="VD"><a href="http://fr.viadeo.com/fr/profile/emilie.hivert" alt="mon viadeo" target="_blank"></a></div>
+                 <div id="Linkedin"><a href="http://lnkd.in/dZv6nyf" alt="mon linkedin" target="_blank"></a></div>
+                 </div-->
+			</div>
+            
+        </div>
+    </div>
